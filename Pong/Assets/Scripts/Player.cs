c6ee42ac26845859f01cbf5b64db40c8de2ff81f
Beau <beau.d.findlay@gmail.com>
2021-02-05 21:51:47 +0000

    Add player movement.

--- Pong/Assets/Scripts/Player.cs
+++ Pong/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 8.0f;
+    public float movementSpeed = 8.0f;
     public float topMovementLimit = 8.3f;
     public float bottomMovementLimit = -8.3f;
     public Vector2 startingPosition = new Vector2(-13f, 0.0f);
@@ -18,6 +18,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckUserInput();
+    }
 
+    void CheckUserInput()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) 
+        {
+            if (transform.localPosition.y > topMovementLimit) 
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, topMovementLimit, transform.localPosition.z);
+            }
+            else 
+            {
+                transform.localPosition += Vector3.up * movementSpeed * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (transform.localPosition.y < bottomMovementLimit)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, bottomMovementLimit, transform.localPosition.z);
+            }
+            else
+            {
+                transform.localPosition += Vector3.down * movementSpeed * Time.deltaTime;
+            }
+        }
     }
 }
