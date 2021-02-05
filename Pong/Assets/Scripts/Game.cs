99223b86b2bd0acd0e7fd2e31c0f89e03627aa43
Beau <beau.d.findlay@gmail.com>
2021-02-05 22:26:24 +0000

    Spawn ball on start.

--- Pong/Assets/Scripts/Game.cs
+++ Pong/Assets/Scripts/Game.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    private GameObject ball;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnBall();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnBall()
+    {
+        ball = GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Ball", typeof(GameObject)));
+        ball.transform.localPosition = new Vector3(12, 0, -2);
     }
 }
