3373f46e0245008765d80206031966904a7c3d98
Beau <beau.d.findlay@gmail.com>
2021-02-05 21:21:57 +0000

    Set player starting point.

--- Pong/Assets/Scripts/Player.cs
+++ Pong/Assets/Scripts/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float moveSpeed = 8.0f;
+    public float topMovementLimit = 8.3f;
+    public float bottomMovementLimit = -8.3f;
+    public Vector2 startingPosition = new Vector2(-13f, 0.0f);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.localPosition = (Vector3)startingPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
